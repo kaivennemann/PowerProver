0c3f3ac5b4fc53f5b5c99da3a916b2d9c9e78bf4
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-07-11 21:32:10 +0200

    /satisfy end point tested and working

--- api/routes.ml
+++ api/routes.ml
@@ -1,9 +1,14 @@
 (* Define the routes for our api *)
 
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+open Power_prover.Exports
 
 type post_request_data = {
   data : string;
+} [@@deriving yojson]
+
+type interpretation = {
+  assignment : (string * bool) list option;
 } [@@deriving yojson]
 
 let routes = [
@@ -21,5 +26,25 @@ let routes = [
       `String "Dummy return data"
       |> Yojson.Safe.to_string
       |> Dream.json);
+
+  Dream.post "/satisfy"
+    (fun request ->
+      let%lwt body = Dream.body request in
+      
+      let input =
+        body
+        |> Yojson.Safe.from_string
+        |> post_request_data_of_yojson
+      in
+      
+      let i = input.data |> parse_input |> find_satisfying_interpretation in
+
+      let response = {
+        assignment = i;
+      } in
+
+      response |> yojson_of_interpretation |> Yojson.Safe.to_string |> Dream.json
+      
+    );
 
 ]
