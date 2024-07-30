12d23d3c89b7c1a84c09f9f07c2ac1e97091b89d
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-07-30 22:18:02 +0200

    added middleware to cicrumvent cors during development

--- api/startapp.ml
+++ api/startapp.ml
@@ -1,7 +1,26 @@
-(* Create and start the app *)
 
+(* Middleware to enable CORS for testing purposes *)
+let cors_middleware handler request =
+  let handlers =
+    [
+      "Allow", "OPTIONS, GET, HEAD, POST";
+      "Access-Control-Allow-Origin", "*";
+      "Access-Control-Allow-Methods", "OPTIONS, GET, HEAD, POST";
+      "Access-Control-Allow-Headers", "Content-Type";
+      "Access-Control-Max-Age", "86400"
+    ]
+  in
+
+  let%lwt res = handler request in
+  handlers
+  |> List.map (fun (key, value) -> Dream.add_header res key value)
+  |> ignore;
+  Lwt.return res
+
+  (* Create and start the app *)
 let startapp () =
   Dream.run
   @@ Dream.logger
-  @@ Dream.origin_referrer_check
+  (* @@ Dream.origin_referrer_check *)
+  @@ cors_middleware (* TODO: remove *)
   @@ Dream.router Routes.routes
